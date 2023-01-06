6d03b45f28705d8b7fc0c47c69ee48d0fdf0ced3
Dmitriy Pavlovich <dima-kotkov2@yandex.ru>
2023-01-06 23:24:42 +0300

    ver 4.0

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class main {
     public static boolean is_it_singleplayer = true;
     public static void main(String[] args) {
-        System.out.println("\tHello! This is the game REVERSI 2.0.\nUse this terminal to choose the quantity of players and to make a move back.\nTo start a game print \"1\".\n");
+        System.out.println("\tHello! This is the game REVERSI 4.0.\nUse this terminal to choose the quantity of players and to make a move back.\nTo start a game print \"1\".\n");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         if (num == 1) {
